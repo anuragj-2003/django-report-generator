6b4c33c451cd18dcf49ff53322decf4c53dcfe62
ANURAG J <93926757+anuragj-2003@users.noreply.github.com>
2024-06-01 16:21:26 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-))qxzlln9&ki+6n(3b6geo8*awy8o=lm0&8in(#qd1tmma$$+@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
